3044cd211f5306b324d2c648b7856b350258a5dd
sDeyra <sdeymt@gmail.com>
2025-07-22 19:45:02 +0000

    Semaforo Terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import '/workspaces/reactTrafficLight-sDeyra/src/styles/index.css';
 
 //create your first component
 const Home = () => {
+	const [red, setRed] = useState("secondary")
+	const [yellow, setYellow] = useState("secondary")
+	const [green, setGreen] = useState("secondary")
+	function prenderRojo() {
+		setRed("danger")
+		setYellow("secondary")
+		setGreen("secondary")
+	}
+	function prenderAmarillo() {
+		setRed("secondary")
+		setYellow("warning")
+		setGreen("secondary")
+	}
+	function prenderVerde() {
+		setGreen("success")
+		setRed("secondary")
+		setYellow("secondary")
+	}
 	return (
 		<div className="text-center">
-            
+			<div id="app">
+				<div className="d-flex justify-content-center">
+					<div id="trafficTop"></div>
+				</div>
+				<div id="container">
+					<div className={`red-light rounded-circle bg-${red}`} onClick={prenderRojo}></div>
+					<div className={`yellow-light rounded-circle bg-${yellow}`} onClick={prenderAmarillo}></div>
+					<div className={`green-light rounded-circle bg-${green}`} onClick={prenderVerde}></div>
+				</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			</div>
+
 		</div>
 	);
 };
